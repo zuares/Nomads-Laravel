d0f04380777ea7e38f3fb93851af87569016c2ea
zuares <zuaresquad@gmail.com>
2020-11-18 02:36:00 +0700

    Layouting admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('Dashboard');
+    });
