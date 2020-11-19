014cb0833499a062feef9a2a721cb08673ea1f2f
zuares <zuaresquad@gmail.com>
2020-11-19 22:01:10 +0700

    Integrasi Halaman Client

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
 <header class="text-center">
     <h1>Explore The Beautiful World <br> As Easy One Click</h1>
     <p class="mt-3">You Will see beautiful <br> moment you never see before</p>
-    <a href="#" class="btn btn-get-started px-4 mt-4">Get Started</a>
+    <a href="#popular" class="btn btn-get-started px-4 mt-4">Get Started</a>
 </header>
 <!-- End of Header -->
 
@@ -46,52 +46,24 @@
         </div>
     </section>
     <!-- Sectiond Populare -->
-    <div class="section-populare-content">
-
+    <div class="section-populare-content" id="populare">
         <div class="container">
             <div class="section-populare-travel row justify-content-center">
-                <div class="col-sm-6 col-md-4 col-lg-3">
+                @foreach ($items as $item)
+
+                <div class="col-sm-6 col-md-4 col-lg-3 ">
                     <div class="card-travel text-center d-flex flex-column"
-                        style="background-image: url({{url('frontend/images/populare\ 2.jpg')}});">
-                        <div class="travel-country">INDONESIA</div>
-                        <div class="travel-locartion">DERATAN, BALI</div>
+                        style="background-image: url('{{$item->galleries->count()?  asset($item->galleries->first()->image) :  ""}}');">
+                        <div class="travel-country">{{$item->location}}</div>
+                        <div class="travel-location">{{$item->title}}</div>
                         <div class="travel-btn mt-auto">
-                            <a href="{{route('detail')}}" class="btn btn-travel-details px-4">View Details</a>
+                            <a href="{{route('detail', $item->slug)}}" class="btn btn-travel-details px-4">View
+                                Details</a>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-sm-6 col-md-4 col-lg-3 ">
-                    <div class="card-travel text-center d-flex flex-column"
-                        style="background-image: url({{url('frontend/images/populare\ 3.jpg')}});">
-                        <div class="travel-country">INDONESIA</div>
-                        <div class="travel-locartion">NUSA PENINDA</div>
-                        <div class="travel-btn mt-auto">
-                            <a href="{{route('detail')}}" class="btn btn-travel-details px-4">View Details</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-lg-3 ">
-                    <div class="card-travel text-center d-flex flex-column"
-                        style="background-image: url({{url('frontend/images/populare\ 4.jpg')}});">
-                        <div class="travel-country">INDONESIA</div>
-                        <div class="travel-locartion">BROMO, MALANG</div>
-                        <div class="travel-btn mt-auto">
-                            <a href="{{route('detail')}}" class="btn btn-travel-details px-4">View Details</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-lg-3 ">
-                    <div class="card-travel text-center d-flex flex-column"
-                        style="background-image: url({{url('frontend/images/populare\ 1.jpg')}});">
-                        <div class="travel-country">MIDDLE EAST</div>
-                        <div class="travel-locartion">DUBAI</div>
-                        <div class="travel-btn mt-auto">
-                            <a href="{{route('detail')}}" class="btn btn-travel-details px-4">View Details</a>
-                        </div>
-                    </div>
-                </div>
-
+                @endforeach
 
             </div>
         </div>
@@ -175,7 +147,7 @@
             <div class="row">
                 <div class="col-12 text-center">
                     <a href="" class="btn btn-need-help px-4 mt-4 mx-1">I Need Help</a>
-                    <a href="" class="btn btn-get-started px-4 mt-4 mx-1">Get Started</a>
+                    <a href="{{route('register')}}" class="btn btn-get-started px-4 mt-4 mx-1">Get Started</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,14 @@ use Illuminate\Support\Facades\Auth;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/detail', [DetailController::class, 'index'])->name('detail');
-Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
-Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout-success');
+Route::get('/detail/{slug}', [DetailController::class, 'index'])->name('detail');
+
+// Route Checkout
+Route::post('/checkout/{id}', [CheckoutController::class, 'process'])->name('checkout-process')->middleware(['auth', 'verified']);
+Route::get('/checkout/{id}', [CheckoutController::class, 'index'])->name('checkout')->middleware(['auth', 'verified']);
+Route::post('/checkout/create/{detail_id}', [CheckoutController::class, 'create'])->name('checkout-create')->middleware(['auth', 'verified']);
+Route::get('/checkout/remove/{detail_id}', [CheckoutController::class, 'remove'])->name('checkout-remove')->middleware(['auth', 'verified']);
+Route::get('/checkout/confirm/{detail_id}', [CheckoutController::class, 'success'])->name('checkout-success')->middleware(['auth', 'verified']);
 
 
 Route::prefix('admin')
